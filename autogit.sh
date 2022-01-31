b151c01fc5c32dfc5407d353207e30cc343e271a
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-31 10:38:14 -0300

    Next Topic: Sorting rows using column values | L03_Pandas.ipynb -> alts add

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "Next Topic: Sorting rows using column values"
+git commit -m "Next Topic: Sorting rows using column values | L03_Pandas.ipynb -> alts add"
 git push -u origin master
 git status
 exit
